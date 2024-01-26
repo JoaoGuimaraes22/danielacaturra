3a51aea159ca277980d97f621e54ac4929eba5c3
João Guimarães <jssgmrs22@gmail.com>
2024-01-26 15:01:31 +0000

    homepage name change

--- src/index.js
+++ src/index.js
@@ -37,10 +37,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {" "}
     <HashRouter>
-      {" "}
-      <App />{" "}
-    </HashRouter>{" "}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
