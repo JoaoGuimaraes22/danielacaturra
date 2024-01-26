b9e045fd26b3383f3513c0853eb08ba8132c73f0
João Guimarães <jssgmrs22@gmail.com>
2024-01-26 16:37:04 +0000

    Resize

--- src/pages/about.js
+++ src/pages/about.js
@@ -50,12 +50,12 @@ const About = () => {
           <p>Experience</p>
         </div>
         <div className="sub_c ">
-          <div className="exp_block">
+          <div className="block_1">
             <p>Sep. 2023 — Dec. 2023</p>
             <p>MinimalDEsign (Brescia, Italy // Erasmus + Programme)</p>
             <p>Graphic Designer</p>
           </div>
-          <div className="exp_block">
+          <div className="block">
             <p>2020 — 2023</p>
             <p>Freelancer</p>
             <p>Logo for ‘Chuncky Island’</p>
@@ -65,17 +65,17 @@ const About = () => {
               Africana)
             </p>
           </div>
-          <div className="exp_block">
+          <div className="block">
             <p>Oct. 2022 — Dec. 2022</p>
             <p>38 graus</p>
             <p>Communication Designer (Social Media Content)</p>
           </div>
-          <div className="exp_block">
+          <div className="block">
             <p>Sep. 2021 — Dec. 2021</p>
             <p>APEEN</p>
             <p>Web designer; Collaborative work</p>
           </div>
-          <div className="exp_block">
+          <div className="block">
             <p>Sep. 2020 — Jan. 2021</p>
             <p>Carca Crew Clothing</p>
             <p>Graphic Designer & Photoshoot Coordinator</p>
@@ -89,19 +89,19 @@ const About = () => {
           <p>Education</p>
         </div>
         <div className="sub_c">
-          <div className="edu_block">
+          <div className="block_1">
             <p>2022 — 2023</p>
             <p>ETIC, BA (Hons) Graphic Design</p>
           </div>
-          <div className="edu_block">
+          <div className="block">
             <p>2020 — 2022 </p>
             <p>ETIC, HN Diploma in Communication and Multimedia Design</p>
           </div>
-          <div className="edu_block">
+          <div className="block">
             <p>April 2020</p>
             <p>Lisbon Digital School Intensive Marketing Digital Course</p>
           </div>
-          <div className="edu_block">
+          <div className="block">
             <p>2016 — 2019 </p>
             <p>Escola Secundária Quinta do Marquês (Humanistic Area) </p>
           </div>
@@ -114,7 +114,7 @@ const About = () => {
           <p>Workshop</p>
         </div>
         <div className="sub_c">
-          <div className="work_block">
+          <div className="block_1">
             <p>2023</p>
             <p>Riso Printing, Desisto</p>
           </div>
